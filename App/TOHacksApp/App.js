c620d422bbc3ab405b1a89f9ca05163abf0f24b0
Uzair Ahmed <uzairmahmed@gmail.com>
2020-05-02 14:11:19 -0400

    Setup template code for app.js

--- App/TOHacksApp/App.js
+++ App/TOHacksApp/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+
+import { View, Text, Card, Button } from 'react-native-ui-lib';
+
+import { NativeRouter, Route, Link } from "react-router-native";
+
+import LoginPage from "./components/LoginPage.js"
+import DashboardPage from "./components/DashboardPage.js"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+      <NativeRouter>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/dash" component={DashboardPage} />
+      </NativeRouter>
+    );
 }
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 25,
+    padding: 10
   },
+  header: {
+    fontSize: 20
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  navItem: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10
+  },
+  subNavItem: {
+    padding: 5
+  },
+  topic: {
+    textAlign: "center",
+    fontSize: 15
+  }
 });
