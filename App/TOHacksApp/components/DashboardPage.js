87d374278beb5d0c0edb824c9644feeb48bbc7a1
Niranjan Krishnaswamy <niranjan.k0609@gmail.com>
2020-05-02 16:07:06 -0400

    edit

--- App/TOHacksApp/components/DashboardPage.js
+++ App/TOHacksApp/components/DashboardPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; //some lines
+import React, { useState } from 'react'; //some
 
 import { TouchableOpacity, FlatList } from "react-native";
 import { View, Text, TextField, Card, Button, Image } from 'react-native-ui-lib';
