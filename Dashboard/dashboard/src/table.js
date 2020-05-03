975dc8e9ba969df8d8fe49a05a47c63ebc85d8b7
Niranjan Krishnaswamy <niranjan.k0609@gmail.com>
2020-05-02 22:03:25 -0400

    fixed the width of the table

--- Dashboard/dashboard/src/table.js
+++ Dashboard/dashboard/src/table.js
@@ -4,13 +4,13 @@ import "./App.css";
 function Table(props) {
     return (
         <div className="table">
-        <table style={{"borderBottom":"1px solid black", "borderCollapse": "collapse"}}>
+        <table style={{"borderBottom":"1px solid black", "borderCollapse": "collapse", width: "100%"}}>
             <thead>
                 <tr>
                     <th style={{"border":"1px solid black"}}align = "center">Name</th>
                     <th style={{"border":"1px solid black"}}align = "center">Time</th>
                 </tr>
-                
+
             </thead>
             <tbody>
                 {
