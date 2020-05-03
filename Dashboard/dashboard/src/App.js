89b5cd6684621b1346ba6eef404fc7a95a4d2fcf
Niranjan Krishnaswamy <niranjan.k0609@gmail.com>
2020-05-03 03:22:30 -0400

    added placeholder qr code, added variable for the number of minutes and let it equal 7 (random number) as a place holder

--- Dashboard/dashboard/src/App.js
+++ Dashboard/dashboard/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Title from "./title";
+import Label from "./label";
+import Bodytext from "./bodytext";
 import Table from "./table";
 import "./App.css";
 import data from "./data/customers.json";
+import qr from "./qrcode.PNG";
 
 class App extends React.Component {
   constructor(props) {
@@ -15,6 +18,9 @@ class App extends React.Component {
   return (
     <div className="app">
       <Title />
+      <Label />
+      <Bodytext />
+      <img className="qrcode" align="right" style={{width: "300px"}} src={qr} alt="Scannable QR Code for the store"/>
       <Table data={this.state.data}/>
     </div>
   );    
--- Dashboard/dashboard/src/title.js
+++ Dashboard/dashboard/src/title.js
@@ -4,9 +4,7 @@ import "./App.css";
 function Title(){
     return (
         <div className="title">
-            <marquee>
                 <h2>Our Product</h2>
-                </marquee>
         </div>
     );
 }
